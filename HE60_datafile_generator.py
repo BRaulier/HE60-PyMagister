b1eb7ab4eb60ee3654e36140f9c0e9fdc4ddef1c
Bastian Raulier <braulier@Bastians-MacBook-Air.local>
2021-11-24 22:24:13 -0500

    run_me.py now contains a functionnal example, based on mobley et al. 1998)

--- HE60_datafile_generator.py
+++ HE60_datafile_generator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import shutil
 
 
 def create_null_pure_water():
@@ -18,9 +20,48 @@ def create_null_pure_water():
         file.write(footer)
 
 
-class Simulation:
-    def __init__(self):
-        #simbuild = EnvironmentBuilder()
+class EnvironmentBuilder:
+
+    def create_simulation_environnement(self, path):
+        self.create_backscattering_file(path)
+        self.create_ac9_file(path)
+
+    def create_backscattering_file(self, path):
+        header = "\\begin_header\n" \
+                 "Backscattering file used to find a corresponding Fournier-Forand phase \n" \
+                 "function. As described in https://www.oceanopticsbook.info/view/scattering/the-fournier-forand-phase-function\n" \
+                 "Column headers (depth in m and bb in 1/m):\n" \
+                 "depth {} ".format('\tbb'.join([str(int(i)) for i in self.wavelengths])) + "\n" \
+                 "The first data record gives the number of wavelengths and the wavelengths.\n" \
+                 "\\end_header\n"
+        first_line = "{}\t{}\n".format(int(self.n_wavelengths), '\t'.join([str(i) for i in self.wavelengths]))
+        footer = "\\end_data"
+        with open(path+'/backscattering_file.txt', 'w') as file:
+            file.write(header)
+            file.write(first_line)
+            np.savetxt(file, self.z_bb_grid, fmt='%1.2e', delimiter='\t')
+            file.write(footer)
+        shutil.copy(src=path+'/backscattering_file.txt', dst=r'/Applications/HE60.app/Contents/data/phase_functions/HydroLight/user_defined/backscattering_file.txt')
+
+    def create_ac9_file(self, path):
+        header = "\\begin_header\n" \
+                 "ac9 file used to describe the arbitrary chosen medium \n" \
+                 "Column headers (depth in m; and and c in 1/m): \n" \
+                 "depth {} {} ".format('a\t'.join([str(int(i)) for i in self.wavelengths]),
+                                       '\tc'.join([str(int(i)) for i in self.wavelengths])) + "\n" \
+                 "The first data record gives the number of wavelengths and the wavelengths.\n" \
+                 "\\end_header\n"
+        first_line = "{}\t{}\n".format(int(self.n_wavelengths), '\t'.join([str(i) for i in self.wavelengths]))
+        footer = "\\end_data"
+        with open(path+'/ac9_file.txt', 'w') as file:
+            file.write(header)
+            file.write(first_line)
+            np.savetxt(file, self.z_ac_grid, fmt='%1.2e', delimiter='\t')
+            file.write(footer)
+
+
+class Simulation(EnvironmentBuilder):
+    def __init__(self, path):
         self.wavelengths = None
         self.n_wavelengths = None
         self.z_max = None
@@ -28,21 +69,28 @@ class Simulation:
         self.z_grid = None
         self.z_ac_grid = None
         self.z_bb_grid = None
+        self.path = path
 
-    def run_mobley_1998_example(self):
+        self.ac9_path = self.path + '/ac9_file.txt'
+        self.bb_path = '/Applications/HE60.app/Contents/data/phase_functions/HydroLight/user_defined/backscattering_file.txt'
+
+    def build_mobley_1998_example(self):
         """
         Four-layer model of sea ice, from Mobley et al. 1998 : MODELING LIGHT PROPAGATION IN SEA ICE
         """
+        self.set_wavelengths(wvelgths=[412.0, 440.0, 488.0, 510.0, 532.0, 555.0, 650.0, 676.0, 715.0])
+        self.set_z_grid(z_max=2.0)
         self.add_layer(z1=0.0, z2=0.1, abs=0.4, scat=250, bb=0.0109)
         self.add_layer(z1=0.1, z2=1.61, abs=0.4, scat=200, bb=0.0042)
         self.add_layer(z1=1.61, z2=1.74, abs=1.28, scat=200, bb=0.0042)
         self.add_layer(z1=1.74, z2=self.z_max+1, abs=0.5, scat=0.1, bb=0.005)
+        self.create_simulation_environnement(path=self.path)
 
     def add_layer(self, z1, z2, abs, scat, bb):
         c = abs + scat
         self.z_ac_grid[(self.z_ac_grid[:, 0] >= z1) & (self.z_ac_grid[:, 0] < z2), 1: self.n_wavelengths] = abs
         self.z_ac_grid[(self.z_ac_grid[:, 0] >= z1) & (self.z_ac_grid[:, 0] < z2), self.n_wavelengths::] = c
-        self.z_bb_grid[(self.z_bb_grid[:, 0] >= z1) & (self.z_bb_grid[:, 0] < z2), 1::] = bb
+        self.z_bb_grid[(self.z_bb_grid[:, 0] >= z1) & (self.z_bb_grid[:, 0] < z2), 1::] = bb * scat 
 
     def set_z_grid(self, z_max, delta_z=0.01):
         self.z_max = z_max
@@ -57,52 +105,10 @@ class Simulation:
         self.wavelengths = np.array(wvelgths)
 
 
-
-class EnvironmentBuilder:
-    def __init__(self, sim_clss, path='ressources/'):
-        self.path = path
-        self.sim_clss = sim_clss
-
-    def create_backscattering_file(self):
-        header = "\\begin_header\n" \
-                 "Backscattering file used to find a corresponding Fournier-Forand phase \n" \
-                 "function. As described in https://www.oceanopticsbook.info/view/scattering/the-fournier-forand-phase-function\n" \
-                 "Column headers (depth in m and bb in 1/m):\n" \
-                 "depth {} ".format('\tbb'.join([str(int(i)) for i in self.sim_clss.wavelengths])) + "\n" \
-                 "The first data record gives the number of wavelengths and the wavelengths.\n" \
-                 "\\end_header\n"
-        first_line = "{}\t{}\n".format(int(self.sim_clss.n_wavelengths), '\t'.join([str(i) for i in self.sim_clss.wavelengths]))
-        footer = "\\end_data"
-        with open('ressources/backscattering_file.txt', 'w') as file:
-            file.write(header)
-            file.write(first_line)
-            np.savetxt(file, self.sim_clss.z_bb_grid, fmt='%1.2e', delimiter='\t')
-            file.write(footer)
-
-    def create_ac9_file(self):
-        header = "\\begin_header\n" \
-                 "Backscattering file used to find a corresponding Fournier-Forand phase \n" \
-                 "function. As described in https://www.oceanopticsbook.info/view/scattering/the-fournier-forand-phase-function\n" \
-                 "Column headers (depth in m; and and c in 1/m): \n" \
-                 "depth {} {} ".format('a\t'.join([str(int(i)) for i in self.sim_clss.wavelengths]),
-                                       '\tc'.join([str(int(i)) for i in self.sim_clss.wavelengths])) + "\n" \
-                 "The first data record gives the number of wavelengths and the wavelengths.\n" \
-                 "\\end_header\n"
-        first_line = "{}\t{}\n".format(int(self.sim_clss.n_wavelengths), '\t'.join([str(i) for i in self.sim_clss.wavelengths]))
-        footer = "\\end_data"
-        with open('ressources/ac9_file.txt', 'w') as file:
-            file.write(header)
-            file.write(first_line)
-            np.savetxt(file, self.sim_clss.z_ac_grid, fmt='%1.2e', delimiter='\t')
-            file.write(footer)
-
 if __name__ == "__main__":
-    test_1 = Simulation()
+    test_1 = Simulation(path='ressources/')
     test_1.set_wavelengths(wvelgths=[412.0, 440.0, 488.0, 510.0, 532.0, 555.0, 650.0, 676.0, 715.0])
     test_1.set_z_grid(z_max=2.0)
-    test_1.run_mobley_1998_example()
-    test_build = EnvironmentBuilder(sim_clss=test_1)
-    test_build.create_backscattering_file()
-    test_build.create_ac9_file()
+    test_1.build_mobley_1998_example()
 
 
--- batch_maker.py
+++ batch_maker.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 
 
-class Batch:
+class BatchMaker:
     def __init__(self, batch_name):
         self.meta = {'record1': {}, 'record2': {}, 'record3': {}, 'record4': {}, 'record5': {}, 'record6': {},
                      'record7': {}, 'record8': {}, 'record9': {}, 'record10': {}, 'record11': {}, 'record12': {}, 'record13': {}}
@@ -10,6 +10,8 @@ class Batch:
         self.record5_str, self.record6_str, self.record7_str, self.record8_str = None, None, None, None
         self.record9_str, self.record10_str, self.record11_str, self.record12_str, self.record13_str = None, None, None, None, None
 
+        self.ac9_path = None
+        self.bb_path = None
         self.batch_file = None
         self.run_title = None
         self.rootname = None
@@ -20,7 +22,7 @@ class Batch:
 
         self.create_batch_file()
 
-    def create_batch_file(self, path="/Users/braulier/Documents/HE60/run/batch"):
+    def create_batch_file(self, path="/Users/braulier/Documents/HE60/run/batch/"):
         self.batch_file = open(path + self.batch_name+'.txt', "w+")
 
     def set_title(self, title):
@@ -32,7 +34,8 @@ class Batch:
     def set_N_band_waves(self, N_band=15):
         self.Nwave = N_band
 
-    def set_all_records(self):
+    def set_all_records(self, ac9_file_path, bb_file_path):
+        self.ac9_path, self.bb_path = ac9_file_path, bb_file_path
         self.set_record1()
         self.set_record2()
         self.set_record3()
@@ -101,7 +104,7 @@ class Batch:
         self.meta['record5']['5c_line2'] = '2, -666, 440, 1, 0.014'      # Measured IOP line
         # record 5d: Specific absorption data file names
         self.meta['record5']['null_water_file'] = '../data/null_H2Oabsorps.txt'      # Null water properties
-        self.meta['record5']['user_absorption_file'] = 'TODO_ABS_FILE'   # TODO: add null water properties
+        self.meta['record5']['user_absorption_file'] = self.ac9_path  # TODO: add null water properties
         # record 5e: Specific scattering parameters
         self.meta['record5']['5e_line1'] = '0, -999, -999, -999, -999, -999'   # Pure water
         self.meta['record5']['5e_line2'] = '-666, -999, -999, -999, -999, -999'   # Measured IOP line
@@ -110,10 +113,10 @@ class Batch:
         self.meta['record5']['5f_line2'] = 'dummybstar.txt'              # Dummy
         # record 5g: type of concentrations and phase functions
         self.meta['record5']['5g_line1'] = '0,0,550,0.01,0'              # Dummy values phase functions
-        self.meta['record5']['5g_line2'] = '0,0,550,0.01,0'              # Dummy values phase functions
+        self.meta['record5']['5g_line2'] = '2,0,550,0.01,0'              # Dummy values phase functions
         # record 5h: discretized phase functions file names
         self.meta['record5']['5h_line1'] = 'dpf_pure_H2O.txt'               # TODO NULL
-        self.meta['record5']['5h_line2'] = 'dpf_Petzold_avg_particle.txt'   # TODO
+        self.meta['record5']['5h_line2'] = 'user_defined/backscattering_file.txt'   # TODO
 
         self.meta['record5']['string'] = '{ncomp}, {nconc}\n{compconc}\n{5c_line1}\n{5c_line2}\n{null_water_file}\n' \
                            '{user_absorption_file}\n{5e_line1}\n{5e_line2}\n{5f_line1}\n{5f_line2}\n' \
@@ -138,7 +141,7 @@ class Batch:
         self.meta['record8']['iflagsky'] = 2                      # 1: idealized sky, 2 (3): semi analytic, zenith angle or (time and location)
         self.meta['record8']['suntheta'] = 45.0                   # solar zenith angle (degrees)
         self.meta['record8']['sunphi'] = 0.0                      # solar azimuthal angle in degrees relative to the wind direction.
-                                                                  # sunphi = 0.0 is downwind and sunphi = 90.0 places the Sun at a right angle to the wind.
+        self.meta['record8']['nsky'] = 3                                                          # sunphi = 0.0 is downwind and sunphi = 90.0 places the Sun at a right angle to the wind.
         self.meta['record8']['cloud'] = 0.5                       # 0.0: clear sky, 1.0:solid overcast
         # record 8b     # CORRESPONDING TO THE CHOICE OF IFLAGSKY (2), must be changed if you use other sky model ( 1 or 3)
         self.meta['record8']['fjday'] = 180.0                     # Julian day (for earth-sun distance)
@@ -151,7 +154,7 @@ class Batch:
         self.meta['record8']['vi'] = 15                             # average horizontal visibility (km) https://essd.copernicus.org/articles/12/805/2020/
         self.meta['record8']['wsm'] = 6.0                          # average wind speed (m/s) https://essd.copernicus.org/articles/12/805/2020/
         self.meta['record8']['ro3'] = 300                          # ozone (Dobson units) https://ozonewatch.gsfc.nasa.gov/NH.html
-        self.meta['record8']['string'] = '{iflagsky}, {suntheta}, {sunphi}, {cloud}\n' \
+        self.meta['record8']['string'] = '{iflagsky}, {nsky}, {suntheta}, {sunphi}, {cloud}\n' \
                            '{fjday}, {rlat}, {rlon}, {pres}, {am}, {rh}, {wv}, {vi}, {wsm}, {ro3}\n'.format(**self.meta['record8'])
 
     def set_record9(self):
@@ -169,17 +172,17 @@ class Batch:
 
     def set_record11(self):
         self.meta['record11']['iop'] = 0                        # Flag, 0, (1): indicating geometrical (optical) depths
-        self.meta['record11']['nznom'] = 10                     # number of depths
-        self.meta['record11']['zetanom'] = np.linspace(0, 10, self.meta['record11']['nznom']+1, dtype=np.float16)
+        self.meta['record11']['nznom'] = 100                     # number of depths
+        self.meta['record11']['zetanom'] = np.linspace(0, 2, self.meta['record11']['nznom']+1, dtype=np.float16)
         self.meta['record11']['zetanom_str'] = ','.join([str(i) for i in self.meta['record11']['zetanom']])
-        self.meta['record11']['string'] = '{iop}, {nznom}, {zetanom_str}\n'.format(**self.meta['record11'])
+        self.meta['record11']['string'] = '{iop},{nznom},{zetanom_str}\n'.format(**self.meta['record11'])
 
     def set_record12(self):
         self.meta['record12']['PureWaterDataFile'] = self.meta['record5']['null_water_file']
         self.meta['record12']['nac9Files'] = 1                                       # Number of ac9 files to read
-        self.meta['record12']['ac9DataFile'] = self.meta['record5']['user_absorption_file']
+        self.meta['record12']['ac9DataFile'] = self.ac9_path
         self.meta['record12']['Ac9FilteredDataFile'] = 'dummyFilteredAc9.txt'
-        self.meta['record12']['HydroScatDataFile'] = 'TODO BACKSCATTER'                       # TODO:  backscattering data
+        self.meta['record12']['HydroScatDataFile'] = self.bb_path                       # TODO:  backscattering data
         self.meta['record12']['ChlzDataFile'] = 'dummyCHLdata.txt'                       # Standard-format chlorophyll profile
         self.meta['record12']['CDOMDataFile'] = 'dummyCDOMdata.txt'                       # file containing values of CDOM absorption at a given reference wavelength
         self.meta['record12']['RbottomFile'] = 'dummyR.bot'                       # file containing values of CDOM absorption at a given reference wavelength
