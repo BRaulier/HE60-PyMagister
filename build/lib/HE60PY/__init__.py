d49f0a30244d01d5a87849f603eae9fa2bac6f6a
Bastian Raulier <bastian.raulier.1@ulaval.ca>
2022-02-04 12:03:21 -0500

    Delete build/lib directory

--- build/lib/HE60PY/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ac9simulation import AC9Simulation
-
-__version__ = "0.0.1"
-__author__ = 'Bastian Raulier'
--- build/lib/HE60PY/test/test.py
+++
@@ -1,8 +0,0 @@
-# Unit testing
-# 1- Création de fichiers
-
-# Style
-# Faire attention aux underscores
-
-# Un fichier par classe!
-#
