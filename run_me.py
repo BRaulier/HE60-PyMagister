be6c099c803993193d61ab405f98b3493c781afa
Bastian Raulier <braulier@Bastians-MacBook-Air.local>
2022-03-01 20:23:09 -0500

    Custom AC9 example is now working

--- run_me.py
+++ run_me.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     # mobley_sim = AC9Simulation(path=path_to_user_files, run_title='test', root_name='Mobley_1998_example', mode='sea_ice', Nwavel=17)
     # mobley_sim.build_and_run_mobley_1998_example()
 
-    user_built_sim = AC9Simulation(path=path_to_user_files, batch_name='user_built_example')
+    user_built_sim = AC9Simulation(path=path_to_user_files, run_title='test', root_name='Mobley_1998_example', mode='sea_ice', Nwavel=17)
     user_built_sim.set_z_grid(z_max=2.0)
     user_built_sim.add_layer(z1=0.0, z2=0.5, abs=0.5, scat=1000, bb=0.0109)
     user_built_sim.add_layer(z1=0.5, z2=2.01, abs=0.4, scat=200, bb=0.0042)
-    user_built_sim.run_built_model(printOutput=True)
+    user_built_sim.run_simulation(printoutput=True)
